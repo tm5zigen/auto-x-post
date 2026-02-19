5f149800b6c917819a0c31b1dc8e4f9505f3b331
tm5zigen <161125912+tm5zigen@users.noreply.github.com>
2026-02-19 23:01:37 +0900

    Update post.py

--- post.py
+++ post.py
@@ -63,7 +63,7 @@ def post_to_x(text):
         consumer_key=os.environ["API_KEY"],
         consumer_secret=os.environ["API_SECRET"],
         access_token=os.environ["ACCESS_TOKEN"],
-        access_token_secret=os.environ["ACCESS_SECRET"],
+        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
     )
 
     client.create_tweet(text=text)
